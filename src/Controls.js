fbee6f99f64bd4c88bae98a92d08085275f84be1
warrgiff <warrenbruceg@gmail.com>
2020-11-09 20:34:56 -0800

    setting up draggable modal

---
+++ src/Controls.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const Controls = (props) => {
+    return ReactDom.createPortal(
+        <div className="controls_container">
+
+        </div>
+    )
+}
+
+export default Controls
